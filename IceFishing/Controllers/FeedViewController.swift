86f5e8e2abb946b534964c2e055ee4c0468303dd
Lucas Derraugh <lucas.derraugh@me.com>
2015-04-05 00:00:19 -0400

    Add Views folder, fix Swift styling mistakes

--- IceFishing/Controllers/FeedViewController.swift
+++ IceFishing/Controllers/FeedViewController.swift
@@ -32,11 +32,10 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var refreshControler = UIRefreshControl()
         
-        self.refreshControl = refreshControler
-        self.refreshControl?.addTarget(self, action: "didRefreshFeed", forControlEvents: .ValueChanged)
-        self.refreshControl?.attributedTitle = NSAttributedString (string: "Last Updated on \(NSDate())")
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: "refreshFeed", forControlEvents: .ValueChanged)
+        refreshControl?.attributedTitle = NSAttributedString(string: "Last Updated on \(NSDate())")
         
         tableView.separatorStyle = .None
         tableView.registerNib(UINib(nibName: "FeedTableViewCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
@@ -44,7 +43,7 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
     
     //Mark: - UIRefreshControl
     
-    func didRefreshFeed() {
+    func refreshFeed() {
         testSongIDs.append("https://p.scdn.co/mp3-preview/dba0ce6ac6310d7be00545861f9b58aeb86930a3")
         testSongDescriptions.append("Don't Stop the Party - Pitbull")
 
@@ -83,7 +82,7 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
             [unowned self]
             (isPlaying, sender) in
             
-            if (isPlaying) {
+            if isPlaying {
                 
                 if (self.currentlyPlayingCell != sender) {
                     self.currentlyPlayingCell?.player.pause()
@@ -102,19 +101,19 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if (tableView.indexPathForSelectedRow() != nil){ //If a row is selected
+        if tableView.indexPathForSelectedRow() != nil { //If a row is selected
             view.addSubview(pinView)
             //println("lol")
-            var selectedRow = tableView.indexPathForSelectedRow()! //Selected Row
-            var rowsICanSee = tableView.indexPathsForVisibleRows()! //Rows Seen
-            var cellSelected = tableView.cellForRowAtIndexPath(selectedRow)
-            if(rowsICanSee[0] as NSObject == selectedRow || rowsICanSee[rowsICanSee.count-1] as NSObject == selectedRow) { //If the cell is the top or bottom
+            let selectedRow = tableView.indexPathForSelectedRow()! //Selected Row
+            let rowsICanSee = tableView.indexPathsForVisibleRows() as [NSIndexPath] //Rows Seen
+            let cellSelected = tableView.cellForRowAtIndexPath(selectedRow)
+            if rowsICanSee.first == selectedRow || rowsICanSee.last == selectedRow { //If the cell is the top or bottom
                 pinView.backgroundColor = cellSelected!.backgroundColor
-                if(rowsICanSee[0] as NSObject == selectedRow){
+                if rowsICanSee.first == selectedRow {
                     pinView.center = CGPoint(x: view.center.x, y: 50)
                     pinView.removeFromSuperview()
                     view.addSubview(pinView)
-                } else if (rowsICanSee[rowsICanSee.count-1] as NSObject == selectedRow){
+                } else if rowsICanSee.last == selectedRow {
                     pinView.center = CGPoint(x: view.center.x, y: view.frame.height - 50)
                     pinView.removeFromSuperview()
                     view.addSubview(pinView)
@@ -123,7 +122,7 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
             }
                 
             else {
-                if(selectedRow.compare(rowsICanSee[0] as NSIndexPath) != selectedRow.compare(rowsICanSee[rowsICanSee.count-1] as NSIndexPath)) { //If they're equal then the thing is not on screen
+                if selectedRow.compare(rowsICanSee.first!) != selectedRow.compare(rowsICanSee.last!) { //If they're equal then the thing is not on screen
                     pinView.center = CGPoint(x: cellSelected!.center.x, y: cellSelected!.center.y - tableView.contentOffset.y)
                     pinView.backgroundColor = cellSelected!.backgroundColor
                 }
@@ -134,22 +133,22 @@ class FeedViewController: UITableViewController, UIScrollViewDelegate {
     override func scrollViewDidScroll(scrollView: UIScrollView) {
         if let selectedRow = tableView.indexPathForSelectedRow() { //If a row is selected
             view.addSubview(pinView)
-            var rowsICanSee = tableView.indexPathsForVisibleRows()! //Rows Seen
-            var cellSelected = tableView.cellForRowAtIndexPath(selectedRow)
-            if(rowsICanSee[0] as NSObject == selectedRow || rowsICanSee[rowsICanSee.count-1] as NSObject == selectedRow) { //If the cell is the top or bottom
+            let rowsICanSee = tableView.indexPathsForVisibleRows() as [NSIndexPath] //Rows Seen
+            let cellSelected = tableView.cellForRowAtIndexPath(selectedRow)
+            if rowsICanSee.first == selectedRow || rowsICanSee.last == selectedRow { //If the cell is the top or bottom
                 pinView.backgroundColor = cellSelected!.backgroundColor
-                if(rowsICanSee[0] as NSObject == selectedRow){
+                if rowsICanSee.first == selectedRow {
                     pinView.center = CGPoint(x: view.center.x, y: 50)
                     pinView.removeFromSuperview()
                     view.addSubview(pinView)
-                } else if (rowsICanSee[rowsICanSee.count-1] as NSObject == selectedRow){
+                } else if rowsICanSee.last == selectedRow {
                     pinView.center = CGPoint(x: view.center.x, y: view.frame.height - 50)
                     pinView.removeFromSuperview()
                     view.addSubview(pinView)
                 }
             }
             else {
-                if(selectedRow.compare(rowsICanSee[0] as NSIndexPath) != selectedRow.compare(rowsICanSee[rowsICanSee.count-1] as NSIndexPath)) { //If they're equal then the thing is not on screen
+                if selectedRow.compare(rowsICanSee.first!) != selectedRow.compare(rowsICanSee.last!) { //If they're equal then the thing is not on screen
                     pinView.center = CGPoint(x: cellSelected!.center.x, y: cellSelected!.center.y - tableView.contentOffset.y)
                     pinView.backgroundColor = cellSelected!.backgroundColor
                 }
