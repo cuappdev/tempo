336496c43389b62fffb629403d9adc98a40c0eee
Jesse Chen <jmc622@cornell.edu>
2016-10-02 16:50:16 -0400

    UI tweaks

--- Tempo/Controllers/AboutViewController.swift
+++ Tempo/Controllers/AboutViewController.swift
@@ -14,12 +14,11 @@ class AboutViewController: UIViewController {
     @IBOutlet weak var linksLabel: UITextView!
 	
     let aboutText = "Cornell App Development is a project team at Cornell University dedicated to mobile app development. Every year, we take an app from idea to product, culminating with a release on the App Store. We give students the opportunity to work in a startup environment and gain practical experience in software development, design, and product management. Our team is made up of talented designers, developers, and visionaries who collaborate to bring projects to life."
-	let teamText = "Leads: Annie Cheng, Andrew Aquino, Dennis Fedorko, Ilan Filonenko, Adam ElShaer \n\n" +
+	let teamText = "Leads: Andrew Aquino, Annie Cheng, Adam ElShaer, Dennis Fedorko, Ilan Filonenko \n\n" +
 		"Designers: Austin Chan, Mihir Chauhan, Sara Cheong, Eileen Dai, Derrick Ho, Sahil Khoja, Jonathan Lee, Jason Wu, Tiffany Zheng \n\n" +
-		"iOS: Logan Allen, Joseph Antonakakis, Natasha Armbrust, Austin Astorga, Matt Barker, Mark Bryan, Jesse Chen, Andrew Dunne, Arman Esmaili, Kevin Greer, Daniel Li, Emily Lien, Monica Ong, Keivan Shahida \n\n" +
-		"Backend: Celine Brass, Rishab Gupta, Hong Jeon, Sanjana Kaundinya, Ji Hun Kim, Amit Mizrahi, Shiv Roychowdhury"
+		"iOS: Logan Allen, Natasha Armbrust, Austin Astorga, Matt Barker, Mark Bryan, Jesse Chen, Andrew Dunne, Arman Esmaili, Kevin Greer, Daniel Li, Emily Lien, Monica Ong, Keivan Shahida \n\n" +
+		"Backend: Joseph Antonakakis, Celine Brass, Rishab Gupta, Hong Jeon, Sanjana Kaundinya, Ji Hun Kim, Amit Mizrahi, Shiv Roychowdhury"
 	let linksText = "http://www.cuappdev.org/ \n" + "https://github.com/cuappdev/ \n" + "https://twitter.com/cornellappdev/ \n"
-		
 
     override func viewDidLoad() {
         super.viewDidLoad()
