2255e173fa9d5dcebf4b70a72ea53a10f3c51841
Keivan Shahida <keivanshahida@yahoo.com>
2016-11-30 18:53:25 -0500

    Fixed multiple Facebook popups bug

--- Tempo/Controllers/FacebookLoginViewController.swift
+++ Tempo/Controllers/FacebookLoginViewController.swift
@@ -97,18 +97,22 @@ class FacebookLoginViewController: UIViewController {
 	
 	func pressButton(){
 		loginButton.alpha = 0.8
+		loginButton.isUserInteractionEnabled = false
 	}
 	
 	func releaseButton(){
 		loginButton.alpha = 1.0
+		loginButton.isUserInteractionEnabled = true
 	}
 	
 	func loginToFacebook() {
 		showActivityIndicator()
+		loginButton.isUserInteractionEnabled = false
 		loginButton.alpha = 1.0
 		let fbLoginManager = FBSDKLoginManager()
 		fbLoginManager.logOut()
 		fbLoginManager.logIn(withReadPermissions: ["public_profile", "email", "user_friends"], from: nil) { loginResult, error in
+			self.loginButton.isUserInteractionEnabled = true
 			if error != nil {
 				print("Facebook login error: \(error)")
 				self.hideActivityIndicator()
