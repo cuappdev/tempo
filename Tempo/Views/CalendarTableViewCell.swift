629399cb1b97d8249d0d3cb5767264040330ab3d
Annie Cheng <ac962@cornell.edu>
2016-12-05 23:11:22 -0500

    Update UI for post history calendar

--- Tempo/Views/CalendarTableViewCell.swift
+++ Tempo/Views/CalendarTableViewCell.swift
@@ -25,15 +25,11 @@ class CalendarTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollec
         super.awakeFromNib()
 		
 		selectionStyle = .none
-		backgroundColor = .clear
+		contentView.backgroundColor = .profileBackgroundBlack
     }
 	
 	func setUpCalendarCell(vc: UICollectionViewDataSource) -> UICollectionView {
 		if !collectionViewAdded {
-			let divider = UIView(frame: CGRect(x: bounds.width/11, y: 0, width: 1, height: bounds.height))
-			divider.backgroundColor = .tempoRed
-			addSubview(divider)
-			
 			let layout = HipStickyHeaderFlowLayout()
 			layout.sectionInset = UIEdgeInsets(top: 0, left: padding*6, bottom: padding*2, right: 0)
 			layout.minimumInteritemSpacing = 0
@@ -42,14 +38,18 @@ class CalendarTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollec
 			calendarCollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height), collectionViewLayout: layout)
 			calendarCollectionView.delegate = self
 			calendarCollectionView.dataSource = vc
-			calendarCollectionView.backgroundColor = .clear
+			calendarCollectionView.backgroundColor = .profileBackgroundBlack
 			calendarCollectionView.scrollsToTop = false
 			calendarCollectionView.register(HipCalendarCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Header")
 			calendarCollectionView.register(HipCalendarDayCollectionViewCell.self, forCellWithReuseIdentifier: "DayCell")
 			addSubview(calendarCollectionView)
 			
+			let divider = UIView(frame: CGRect(x: bounds.width/11, y: 0, width: 1, height: bounds.height))
+			divider.backgroundColor = .tempoRed
+			addSubview(divider)
+			
 			collectionViewAdded = true
-		}
+		} 
 		
 		return calendarCollectionView
 	}
